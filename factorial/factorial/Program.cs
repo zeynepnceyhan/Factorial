40e8c2dbeecb816f13e3bec568e350e771da28a7
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-03 11:59:34 +0300

    factorial
    internship

---
+++ factorial/factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double number = Convert.ToInt32(Console.ReadLine());
+            double factorial = 1;
+
+            for(int i = 1; i <= number; i++)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine("The factorial of {0}, is {1}", number, factorial);
+            Console.ReadLine();
+
+        }
+    }
+}
